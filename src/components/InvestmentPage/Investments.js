681c467f9b0f0811589e4605e1c914c086f82b96
oemuraye <oemuraye@gmail.com>
2024-07-19 10:14:05 +0100

    navbar and hero fixing

--- src/components/InvestmentPage/Investments.js
+++ src/components/InvestmentPage/Investments.js
@@ -12,13 +12,14 @@ import './investments.css';
 import ProjectCards from './ProjectCards';
 
 const projectCardDetails = [
-  { title: 'A19', numOfUnits: '19 Plots', annualCashFlow: '60+', description: '1.5X - 3X', years: '3', amount: '$5,000', fullAmount: '$5,000', address: 'Lugbe, Abuja.', projImg: projectImg0, projectStatus: 'Coming Soon'},
-  { title: 'A25', numOfUnits: '25 Units', annualCashFlow: '10%', description: '2X - 3.5X', years: '5', amount: '₦45M', fullAmount: '₦45,000,000', address: 'Lugbe, Abuja.', projImg: projectImg1, projectStatus: 'Closed'},
-  { title: 'A6', numOfUnits: '6 Units', annualCashFlow: '6%%', description: '2.5X - 3.5X', years: '5', amount: '₦30M', fullAmount: '₦30,000,000', address: 'Jahi, Abuja.', projImg: projectImg2, projectStatus: 'Closed'},
-  { title: 'A5', numOfUnits: '5 Units', annualCashFlow: '5-25%', description: '2.5X - 3.5X', years: '5', amount: '₦90M', fullAmount: '₦90,000,000', address: 'Ikota, Lekki Lagos.', projImg: projectImg3, projectStatus: 'Closed'},
+  { title: 'A19', numOfUnits: '60+ Plots', annualCashFlow: '60', description: '1.5X - 3X', years: '3', amount: '$5,000', fullAmount: '$5,000', address: 'Lugbe, Abuja.', projImg: projectImg0, projectStatus: 'Coming Soon'},
+  { title: 'A25', numOfUnits: '25 Units', annualCashFlow: '10', description: '2X - 3.5X', years: '5', amount: '₦45M', fullAmount: '₦45,000,000', address: 'Lugbe, Abuja.', projImg: projectImg1, projectStatus: 'Closed'},
+  { title: 'A6', numOfUnits: '6 Units', annualCashFlow: '6', description: '2.5X - 3.5X', years: '5', amount: '₦30M', fullAmount: '₦30,000,000', address: 'Jahi, Abuja.', projImg: projectImg2, projectStatus: 'Closed'},
+  { title: 'A5', numOfUnits: '5 Units', annualCashFlow: '5-25', description: '2.5X - 3.5X', years: '5', amount: '₦90M', fullAmount: '₦90,000,000', address: 'Ikota, Lekki Lagos.', projImg: projectImg3, projectStatus: 'Closed'},
   { title: 'A6', numOfUnits: '6 Hectare', annualCashFlow: '', description: '3.5X - 10X', years: '10', amount: '₦10M', fullAmount: '₦10,000,000', address: 'Kwali, Abuja.', projImg: projectImg4, projectStatus: 'Closed'},
-  { title: 'A7', numOfUnits: '7 Units', annualCashFlow: '5%', description: '2X - 10X', years: '5', amount: '₦80M', fullAmount: '₦80,000,000', address: 'Ajah, Lagos.', projImg: projectImg5, projectStatus: 'Closed'},
-  { title: 'A25', numOfUnits: '50 Units', annualCashFlow: '10%', description: '2X - 3.5X', years: '5', amount: '₦60M', fullAmount: '₦60,000,000', address: 'Kubwa, Abuja.', projImg: projectImg6, projectStatus: 'Closed'},
+  { title: 'A7', numOfUnits: '7 Units', annualCashFlow: '5', description: '2X - 10X', years: '5', amount: '₦80M', fullAmount: '₦80,000,000', address: 'Ajah, Lagos.', projImg: projectImg5, projectStatus: 'Closed'},
+  { title: 'A25', numOfUnits: '50 Units', annualCashFlow: '10', description: '2X - 3.5X', years: '5', amount: '₦60M', fullAmount: '₦60,000,000', address: 'Kubwa, Abuja.', projImg: projectImg6, projectStatus: 'Closed'},
+  // { title: '', numOfUnits: '', annualCashFlow: '', description: '', years: '', amount: '', fullAmount: '₦', address: '', projImg: projectImg6, projectStatus: ''},
 ]
 
 const Investments = () => {
