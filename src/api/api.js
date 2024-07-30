b0934105df62bfc2a611984db4132ef20a3977fa
oemuraye <oemuraye@gmail.com>
2024-07-30 07:19:31 +0100

    api endpoint fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API = axios.create({ baseURL: 'http://localhost:5000/api' });
-const API = axios.create({ baseURL: 'http://localhost:5000/api/' });
+const API = axios.create({ baseURL: 'https://adesina.onrender.com/api' });
+// const API = axios.create({ baseURL: 'http://localhost:5000/api/' });
 
 API.interceptors.request.use((request) => {
     return request;
